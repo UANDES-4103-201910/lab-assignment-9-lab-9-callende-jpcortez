9442b82c4a09932ec634af2f73da10505acc8284
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2018-04-16 18:12:20 -0300

    Create User Contrller and View Basic. Create Session Controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,10 @@
+class SessionController < ApplicationController
+
+	def new
+		
+	end
+
+	def destroy
+		
+	end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.fetch(:user, {})
+    end
+end
