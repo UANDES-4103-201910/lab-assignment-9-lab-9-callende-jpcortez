f5b67e19ecb2853daac2b057f24e7a6772b8bdc2
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2018-04-16 23:46:48 -0300

    Edit routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
-  root :to => 'application#index'
+  root :to => 'sessions#new'
 
-  get '/log_in' => 'sessions#new', as: :log_in
   post '/login',   to: 'sessions#create'
   delete '/log_out' => 'sessions#destroy', as: :log_out
 
