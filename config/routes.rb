7b759b47367fe8223170f15d37e11be104342a64
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2018-04-17 09:33:42 -0300

    Edit views registration and session, create new routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   root :to => 'sessions#new'
 
-  post '/login',   to: 'sessions#create'
+  post '/login',   to: 'sessions#create', as: :log_in
   delete '/log_out' => 'sessions#destroy', as: :log_out
 
-  get '/sign_in' => 'registrations#new', as: :sign_in
+  get '/sign_in' => 'registrations#new', as: :registrations
+  post '/sign_in' => 'registrations#create', as: :sign_in
+
 end
