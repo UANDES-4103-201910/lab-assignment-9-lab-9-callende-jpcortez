aa333aeb36ae37159e9fdac8c8fdca9ee312bed7
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2018-04-16 22:44:43 -0300

    Fix Sessions name

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
 
   root :to => 'application#index'
 
-  get '/log_in' => 'session#new', as: :log_in
+  get '/log_in' => 'sessions#new', as: :log_in
   post '/login',   to: 'sessions#create'
-  delete '/log_out' => 'session#destroy', as: :log_out
+  delete '/log_out' => 'sessions#destroy', as: :log_out
 
   get '/sign_in' => 'registration#new', as: :sign_in
-  delete '/sign_out' => 'session#destroy', as: :sign_out
 end
